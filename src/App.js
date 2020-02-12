b4816f5e8cdf797a48c6f886b3046080ad176768
Divyanshu Rai <divyanshu1997@gmail.com>
2020-02-12 21:12:32 +0530

    Formed the signin and sign up page with a sign up component having form input and a custom button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import ShopPage from './pages/shop/shop.component';
 
 import Header from './components/header/header.component';
 
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
+
       </Switch>
     </div>
   );
