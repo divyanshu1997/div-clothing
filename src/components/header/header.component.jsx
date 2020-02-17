ac49c7a21b538c7ba94636fea235e8356be5e2b4
Divyanshu Rai <divyanshu1997@gmail.com>
2020-02-18 01:47:30 +0530

    Added redux to the project. Added the user actions and created the cart redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
+import {connect} from 'react-redux';
+
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import {auth} from '../../firebase/firebase.utils';
 
+import ShopIcon from '../cart-icon/cart-icon.component';
+
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss'
 
-const Header =  ({currentUser}) =>(
+const Header =  ({currentUser,hidden}) =>(
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'></Logo>
@@ -22,10 +28,17 @@ const Header =  ({currentUser}) =>(
                 :
                 (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
+            <ShopIcon/>
         </div>
+        { hidden? null : <CartDropdown></CartDropdown>}
     </div>
 
 
 )
 
-export default Header ;
+const mapStateToProps = ({user:{currentUser},cart : {hidden}}) => ({
+    currentUser ,
+    hidden
+});
+
+export default connect(mapStateToProps)(Header) ;
